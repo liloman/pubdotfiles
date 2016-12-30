e653eecf2b9c356d86b4411b31b78141248ef032
liloman <liloman@users.noreply.github.com>
2016-12-30 20:30:11 +0100

    pause only when started :P

--- Scripts/Scripts/lanza_teeworlds.sh
+++ Scripts/Scripts/lanza_teeworlds.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-pomodoro-client.py pause
+started=0
+status=$(pomodoro-client.py status)
+[[ $status == started* ]] && started=1
+((started)) && pomodoro-client.py pause
 timew start teeworlds +nowork
 /home/charly/Descargas/DDNet/DDNet
 timew stop
-pomodoro-client.py pause
+((started)) && pomodoro-client.py pause
