4944a9ba95dcf0e3902ee4231b6be7320567c536
liloman <liloman@users.noreply.github.com>
2017-02-07 16:36:48 +0100

    disable notifications while playing

--- Scripts/Scripts/lanza_teeworlds.sh
+++ Scripts/Scripts/lanza_teeworlds.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
+#set the enviroment before/after teeworlds
 
 pidof DDNet && { echo "$0 already running"; exit 1;  }
 
-started=0
+declare -i started=0
+
+#disable notifications
+killall notification-daemon
+#get status
 status=$(pomodoro-client.py status)
 [[ $status == started* ]] && started=1
+#only pause if started
 ((started)) && pomodoro-client.py pause
+#track teeworlds
 timew start teeworlds +nowork
+#launch it
 /home/charly/Descargas/DDNet/DDNet
+#untrack teeworlds
 timew stop
+#if started then resume it
 ((started)) && pomodoro-client.py pause
+#enable notifications
+nohup /usr/libexec/notification-daemon >/dev/null &
