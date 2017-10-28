af2b76ec3cdd6c3d3c2148e05e4a8331e513b4b5
liloman <liloman@users.noreply.github.com>
2017-10-28 11:58:45 +0200

    w-up  to show current spotify song

--- Scripts/Scripts/current_spotify_song.sh
+++ Scripts/Scripts/current_spotify_song.sh
@@ -1,29 +1,25 @@
 #!/usr/bin/env bash
-#Show current song from a Spotify generated file from a Windows VirtualBox
+#Show current song with icon from dbus Spotify
 
 . ~/Scripts/libnotify
 
 current_spotify_song() {
-    local file=/tmp/.spotify/title.txt
-    local title= artist= song=
-    [[ -d ${file%/*} ]] || mkdir ${file%/*}
-    [[ -e $file ]] || touch $file
-    while true; do 
-        if inotifywait -e modify $file; then
-            #let's give chance to release the file to the batch (sync)
-            sleep 2
-            read -r title < $file
-            title=${title//^\"/}
-            #ctl-v + ctl-m  not ^M!
-            title=${title///}
-            [[ $title == Spotify ]] && { notify "PAUSED!" folder-music ; continue; }
-            artist=${title%%-*}
-            song=${title#*-}
-            echo "title:$title artist:$artist song:$song"
-            # covert art from http://www.last.fm/music/Joe+Farrell/_/Follow+Your+Heart
-            notify "$title" folder-music
-        fi
-    done
+    local resul=$(dbus-send --print-reply --session --type=method_call \
+        --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2  \
+        org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
+
+    local img=$(echo "$resul" |egrep -A 1 "mpris:artUrl"|egrep -v "mpris:artUrl"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$)
+    echo "$img"
+    local artist=$(echo "$resul" |egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$)
+    echo "$artist"
+    local album=$(echo "$resul" |egrep -A 2 "album"|egrep -v "album"|egrep -v "array"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$)
+    echo "($album)\n"
+    local title=$(echo "$resul" |egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$)
+    echo "$title"
 }
 
-current_spotify_song
+mapfile -t res <<< "$(current_spotify_song)"
+wget "${res[0]}" -O /tmp/cover.jpg -q
+
+eval notify-send -t 5000 --hint=int:transient:1 -i /tmp/cover.jpg "'${res[1]}'" "'${res[@]:2}'"
+
