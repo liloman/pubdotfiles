3eb48e317dd08becd6e951424141e862b0055590
liloman <liloman@users.noreply.github.com>
2019-04-15 21:57:58 +0200

    Cambios para med

--- Scripts/Scripts/med.sh
+++ Scripts/Scripts/med.sh
@@ -21,16 +21,15 @@ prana () {
 
 wim () {
     local -i i=0
-    notify_dark "$1" " ..."
     repeat "1->Tummo! " 120
     notify_dark "Exhalaaaaaaaa" " ..."
-    read -p "2->Comienzo a esperar!" pause
+    read -p "2->Exhalo y comienzo a esperar!" pause
     start=$SECONDS
     for(( i = 0; i < 300; i++ )) { echo -ne "$i\r"; read -p "Duracion: " -N 1 -t 1 end; [[ $end ]] && i=3000; }
     echo "Duracion $((SECONDS - start))."
     echo "3->Inhala y aguanta"
     sleep 18
-    echo "4->Exhala y preparate para la siguiente iteracion"
+    notify_dark "4->Exhala y preparate para la siguiente iteracion" "..."
     sleep 20
 }
 
@@ -51,3 +50,5 @@ elif [[ $1 == wim ]]; then #wim hoff
         wim 
     done
 fi
+
+notify_dark "Doneee $1" " Well done!"
